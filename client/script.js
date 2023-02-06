7e45e2e7cd8ce37f2754908e6e6c37ac5fc61c4b
VitaGladka <vita.gladka@gmail.com>
2023-02-06 15:59:33 +0200

    update the server url

--- client/script.js
+++ client/script.js
@@ -74,7 +74,7 @@ async function handleSubmit(e) {
   const messageDiv = document.getElementById(uniqueId);
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://amplify-8m44.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
